6642a2ee8baee6a14c81fbfc96dd242d2d1fcb71
Elurr <jonatikkan@gmail.com>
2022-09-30 13:54:21 +0200

    banner básico terminado (auto)

--- frontend/web dinamica/script-vanilla.js
+++ frontend/web dinamica/script-vanilla.js
@@ -1,3 +1,4 @@
+// MODAL_FORM LOGIN
 // Botón que abra un formulario modal
 const btnLogin = document.getElementById('btnLogin');
 const modalForm = document.getElementById('loginForm');
@@ -23,3 +24,40 @@ window.onclick = function(event) {
         modalForm.style.display = 'none';
     }
 }
+
+//////////////////////////////////////////////////////////////
+// BANNER_CARRUSEL
+
+// Automatizar banner
+
+// declaramos un array con los contenedores de las imgs
+const carrete = document.getElementsByClassName('carousel-img');
+// declaramos un índice para el array carrete
+let slideIndex = 0;
+
+function showSlides() {
+ 
+    // limpiamos el carrete
+    for (let i = 0; i < carrete.length; i++) {
+        carrete[i].style.display = 'none';
+        carrete[i].classList.remove('fade');
+    }
+
+    // mostrar la siguiente img
+    if (slideIndex >= carrete.length) {
+        // si el contador supera la longitud del carrete, se reinicia
+        slideIndex = 0;
+    }
+    carrete[slideIndex].style.display = 'block';
+    carrete[slideIndex].classList.add('fade');
+
+    // aumentamos el contador del índice del carrete
+    slideIndex++;
+
+    // le damos un timeout para que se repita
+    // setTimeout(showSlides, 3000);
+    // cambia de imagen cada 3 segundos
+}
+
+// arrancamos la función
+showSlides();
